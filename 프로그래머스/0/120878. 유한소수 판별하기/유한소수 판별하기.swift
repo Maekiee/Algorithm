684b774c11433caf99378fb79340c061525bde82
Dowon Park <54834533+Maekiee@users.noreply.github.com>
2026-02-14 14:49:16 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func gcd(_ a:Int, _ b:Int) -> Int {
+    if b == 0 { return a }
+    return gcd(b, a % b) 
+}
+
+func solution(_ a:Int, _ b:Int) -> Int {
+    let g = gcd(a, b)
+    var num = abs(b / g)
+    
+    while num % 2 == 0 { num /= 2 }
+    while num % 5 == 0 { num /= 5 }
+    
+    return num == 1 ? 1 : 2
+}
