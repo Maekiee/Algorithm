6e8f0f94144194f2cb319d58e8e8fea2444329a8
Dowon Park <54834533+Maekiee@users.noreply.github.com>
2026-03-07 13:28:47 +0900

    [level 0] Title: 문자열 밀기, Time: 0.08 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/README.md
+++ 프로그래머스/0/120921. 문자열 밀기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 밀기 - 120921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921) 
 
 ### 성능 요약
 
-메모리: 16.3 MB, 시간: 0.09 ms
+메모리: 16.5 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 10일 21:40:23
+2026년 03월 07일 13:28:43
 
 ### 문제 설명
 
--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.swift
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.swift
@@ -1,18 +1,20 @@
 import Foundation
 
 func solution(_ A:String, _ B:String) -> Int {
-    var arr: [String] = A.map { String($0) }
-    var count = 0
-    
-    for _ in 0..<arr.count {
-        if arr.joined() == B {
-            return count
-        }
-        
-        count += 1
-        
-        if let last = arr.popLast() {
-            arr.insert(last, at: 0)
+    var arr = A.map { String($0) }
+    let lastIndex = arr.count - 1
+    var count = 1 
+    if A == B {
+        return 0 
+    } else {
+        while count < arr.count {
+            let lastValue = arr[lastIndex]
+            arr.removeLast()
+            arr.insert(lastValue, at: 0)
+            if arr.joined() == B {
+                return count
+            } 
+            count += 1
         }
     }
     
